127981fcb1e2219238fcb7207dd807711edba1b4
Jhoan Noscue <3106arias@gmail.com>
2026-03-21 17:22:48 +0000

    Interpoblacion de Variables

--- MiPrimerAngular/src/app/app.ts
+++ MiPrimerAngular/src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MiPrimerAngular');
+  bienvenidos = "Hola Jhoan" ;
+
+  Estudiantes = ['Cuadernos','Gafas','Gorras','Lapiceros']
+
 }
+
+
