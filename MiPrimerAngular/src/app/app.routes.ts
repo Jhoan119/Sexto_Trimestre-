1722ab57cde596a9c58611bb8673801096d222f9
Jhoan Noscue <3106arias@gmail.com>
2026-03-27 18:57:39 +0000

    Interpoblacion de variables

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
+import {} from "./pages/inicio/inicio";
+import {} from "./pages/contacto/contacto";
+import {} from "./pages/ejercicio/ejercicio";
 export const routes: Routes = [];
