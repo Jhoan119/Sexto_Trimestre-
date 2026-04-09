10a19f1f85ac0396d699812129ae3fdf81c37c04
Jhoan Noscue <3106arias@gmail.com>
2026-04-09 01:41:47 +0000

    Componentes

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
-import {} from "./pages/inicio/inicio";
-import {} from "./pages/contacto/contacto";
-import {} from "./pages/ejercicio/ejercicio";
-export const routes: Routes = [];
+import {Inicio} from "./pages/inicio/inicio";
+import {Contacto} from "./pages/contacto/contacto";
+import {Ejercicio} from "./pages/ejercicio/ejercicio";
+export const routes: Routes = [
+
+    {
+        path: 'inicio',
+        component: Inicio
+    }
+
+
+];
